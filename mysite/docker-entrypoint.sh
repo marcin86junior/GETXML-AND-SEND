4fcf3bf6b7fdf3761cf76e5cf13e00ec93dd1d0e
marcin86junior <marcin86.design@gmail.com>
2022-06-29 13:11:50 +0200

    docker files corrected

--- mysite/docker-entrypoint.sh
+++ mysite/docker-entrypoint.sh
@@ -4,9 +4,9 @@
 echo "Apply database migrations"
 python manage.py migrate
 
-
-python manage.py migrate --run-syncdb
-python manage.py loaddata group.json users.json data.json
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py makemigrations
 
 # Start server
 echo "Starting server"
